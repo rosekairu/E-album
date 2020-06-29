b37edcd9724896784aaba4b325c6ababda539684
Rose Kairu <rosekairu@gmail.com>
2020-06-29 04:13:03 +0300

    modified database section

--- album/settings.py
+++ album/settings.py
@@ -90,24 +90,15 @@ WSGI_APPLICATION = 'album.wsgi.application'
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
 # development
-if config('MODE')=="dev":
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.postgresql',
-            'NAME': config('DB_NAME'),
-            'USER': config('DB_USER'),
-            'PASSWORD': config('DB_PASSWORD'),
-            'HOST': config('DB_HOST'),
-            'PORT': '',
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': config('DB_NAME'),
+            
         }
     }
-# production
-else:
-   DATABASES = {
-       'default': dj_database_url.config(
-           default=config('DATABASE_URL')
-       )
-   }
+
 
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
